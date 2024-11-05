e91ffaafcae686e0bbfbfb1b53f6011ae60d1e6f
“oscr7” <oscr7r10@outlook.com>
2024-11-04 20:42:11 -0600

    Make generic components. - TopAppBar. - MovieDetail. And remove some imports.

--- app/src/main/java/com/oscar/moviesproject/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/oscar/moviesproject/ui/screens/detail/DetailScreen.kt
@@ -1,30 +1,11 @@
 package com.oscar.moviesproject.ui.screens.detail
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.oscar.moviesproject.R
+import com.oscar.moviesproject.ui.components.DetailTopBar
+import com.oscar.moviesproject.ui.components.MovieDetail
 import com.oscar.moviesproject.ui.components.Screen
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(vm: DetailViewModel, onBack: ()-> Unit) {
     val state = vm.state
@@ -32,39 +13,11 @@ fun DetailScreen(vm: DetailViewModel, onBack: ()-> Unit) {
     Screen {
         Scaffold (
             topBar = {
-                TopAppBar(
-                    title = { Text(text = state.movie?.title ?: "") },
-                    navigationIcon = {
-                       IconButton(onClick = { onBack }) {
-                           Icon(
-                               imageVector = Icons.Default.ArrowBack,
-                               contentDescription = stringResource(id = R.string.back)
-                           )
-                       }
-                    }
-                )
+                DetailTopBar(title = state.movie?.title.orEmpty(), onBack)
             }
-        ){padding ->
-            Column (
-                modifier = Modifier
-                    .padding(padding)
-                    .verticalScroll(rememberScrollState())
-            ){
-                state.movie?.let { movie ->
-                    AsyncImage(
-                        model = movie.poster,
-                        contentDescription = movie.title,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(16 / 9f)
-                    )
-                    Text(
-                        text = movie.title,
-                        modifier = Modifier.padding(16.dp),
-                        style = MaterialTheme.typography.headlineMedium
-                    )
-                }
+        ){ padding ->
+            state.movie?.let { movie->
+                MovieDetail(padding = padding, movie = movie)
             }
         }
     }
--- app/src/main/java/com/oscar/moviesproject/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/oscar/moviesproject/ui/screens/home/HomeScreen.kt
@@ -1,32 +1,23 @@
 package com.oscar.moviesproject.ui.screens.home
 
 import android.Manifest
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -89,7 +80,11 @@ fun HomeScreen(
 }
 
 @Composable
-fun LazyVerticalGridAC(state: HomeViewModel.UiState, contentPadding: PaddingValues, onClick: (Movie)-> Unit) {
+fun LazyVerticalGridAC(
+    state: HomeViewModel.UiState,
+    contentPadding: PaddingValues,
+    onClick: (Movie)-> Unit
+) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(120.dp),
         horizontalArrangement = Arrangement.spacedBy(4.dp),
